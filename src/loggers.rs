dfa1f699a8417f01fd9ad36641ee1e63929ae3b5
luna <ellipticobj@gmail.com>
2025-05-13 23:52:25 +0800

    i thikn fixed output

--- src/loggers.rs
+++ src/loggers.rs
@@ -168,9 +168,8 @@ pub fn printcommitoutput(output: Output, verbose: &u8) {
 
     debug("printing custom commit output", verbose);
     info(&format!(
-        "    {} {}, {} files changed{}",
+        "    {}, {} files changed{}",
         branchinfo.trim(),
-        fileschangedpart.trim(),
         fileschangedcount,
         if let Some(mode) = modeline {
             format!(", {}", mode.trim())
